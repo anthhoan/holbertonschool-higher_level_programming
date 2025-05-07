9b7a14dc4a208567dfbf6b8855c2dbfe400d60f9
anthhoang <anthhoan@gmail.com>
2025-05-07 13:20:47 +1000

    print comb

--- python-if_else_loops_functions/6-print_comb3.py
+++ python-if_else_loops_functions/6-print_comb3.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 
 for tensDigit in range(10):
-    for onesDigit in range (tensDigit + 1, 10):
+    for onesDigit in range(tensDigit + 1, 10):
         if tensDigit == 8 and onesDigit == 9:
             print("{}{}".format(tensDigit, onesDigit), end="\n")
         else:
