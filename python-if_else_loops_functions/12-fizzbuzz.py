b20c5b327f9b34cf1764cd865c7557979f875912
anthhoang <anthhoan@gmail.com>
2025-05-08 21:10:35 +1000

    FizzBuzz

---
+++ python-if_else_loops_functions/12-fizzbuzz.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+def fizzbuzz():
+
+    for number in range(1, 100):
+        if number % 3 == 0 and number % 5 == 0:
+            print("FizzBuzz", end=" ")
+        else:
+            if number % 3 == 0:
+                print("Fizz", end=" ")
+            else:
+                if number % 5 == 0:
+                    print("Buzz", end=" ")
+                else:
+                    print("{}".format(number), end=" ")
