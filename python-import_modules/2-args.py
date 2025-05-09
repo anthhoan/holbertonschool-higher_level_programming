fb3cb1d5a1cf873f3037a961ad3660eed2b40740
anthhoang <anthhoan@gmail.com>
2025-05-09 17:40:40 +1000

    args

---
+++ python-import_modules/2-args.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+
+if __name__ == "__main__":
+    import sys
+
+n = len(sys.argv)
+string = sys.argv
+i = 0
+
+if n > 2:
+    print("{} arguments:".format(n - 1))
+else:
+    if n > 1:
+        print("{} argument:".format(n - 1))
+    else:
+        if n > 0:
+            print("{} arguments.".format(n - 1))
+
+for i in range (1, n):
+    print("{}: {}".format(i, string[i]))
