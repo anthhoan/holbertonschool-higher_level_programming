6a530e87a2049f69e1cc1d05ad0ab61b6da3b205
anthhoang <anthhoan@gmail.com>
2025-05-10 22:33:44 +1000

    import variable

--- python-import_modules/1-calculation.py
+++ python-import_modules/1-calculation.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
 
 if __name__ == "__main__":
-    import calculator_1 as calculation_module
+    from calculator_1 import add, sub, mul, div
 
     a = 10
     b = 5
 
-    print("{} + {} = {}".format(a, b, calculation_module.add(a, b)))
-    print("{} - {} = {}".format(a, b, calculation_module.sub(a, b)))
-    print("{} * {} = {}".format(a, b, calculation_module.mul(a, b)))
-    print("{} / {} = {}".format(a, b, calculation_module.div(a, b)))
+    print("{} + {} = {}".format(a, b, add(a, b)))
+    print("{} - {} = {}".format(a, b, sub(a, b)))
+    print("{} * {} = {}".format(a, b, mul(a, b)))
+    print("{} / {} = {}".format(a, b, div(a, b)))
--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -3,18 +3,18 @@
 if __name__ == "__main__":
     import sys
 
-n = len(sys.argv)
+n = len(sys.argv) - 1
 string = sys.argv
 i = 0
 
-if n > 2:
-    print("{} arguments:".format(n - 1))
+if n >= 2:
+    print("{} arguments:".format(n))
 else:
-    if n > 1:
-        print("{} argument:".format(n - 1))
+    if n == 1:
+        print("{} argument:".format(n))
     else:
-        if n > 0:
-            print("{} arguments.".format(n - 1))
+        if n == 0:
+            print("{} arguments.".format(n))
 
-for i in range(1, n):
+for i in range (1, n + 1):
     print("{}: {}".format(i, string[i]))
