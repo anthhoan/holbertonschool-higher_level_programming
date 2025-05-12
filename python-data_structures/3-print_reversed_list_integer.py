c89be471507396c9578ff31ef895729e10ab8e3d
anthhoang <anthhoan@gmail.com>
2025-05-12 13:20:15 +1000

    copy list

--- python-data_structures/3-print_reversed_list_integer.py
+++ python-data_structures/3-print_reversed_list_integer.py
@@ -4,3 +4,5 @@ def print_reversed_list_integer(my_list=[]):
 
     for i in reversed(my_list):
         print("{:d}".format(i))
+
+# https://stackoverflow.com/questions/3940128/how-do-i-reverse-a-list-or-loop-over-it-backwards
