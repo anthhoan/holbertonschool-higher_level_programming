29792491371e2c3bca45a02d2f39ef1b411b8cfb
anthhoang <anthhoan@gmail.com>
2025-05-12 13:28:28 +1000

    replace

---
+++ python-data_structures/5-no_c.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+
+def no_c(my_string):
+    return my_string.replace('c', '').replace('C', '')
+
+# https://www.geeksforgeeks.org/remove-all-occurrences-of-a-character-in-a-string/
