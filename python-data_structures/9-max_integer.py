389c89c8d2c9e1fa7bb9d217518626e6b04608f3
anthhoang <anthhoan@gmail.com>
2025-05-13 14:53:49 +1000

    largest in list

--- python-data_structures/9-max_integer.py
+++ python-data_structures/9-max_integer.py
@@ -4,6 +4,9 @@ def max_integer(my_list=[]):
 
     largest = my_list[0]
 
+    if my_list == 0:
+        return (None)
+
     for value in my_list:
         if value > largest:
             largest = value
