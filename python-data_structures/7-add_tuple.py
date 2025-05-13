8be46773429dab2f7f5a829286fc096d0994ae62
anthhoang <anthhoan@gmail.com>
2025-05-13 14:40:45 +1000

    add tuple

--- python-data_structures/7-add_tuple.py
+++ python-data_structures/7-add_tuple.py
@@ -1,8 +1,6 @@
 #!/usr/bin/python3
 
 def add_tuple(tuple_a=(), tuple_b=()):
-
-    # if len(tuple_a) != len(tuple_b):
-    #     return (None)
-
-    return (tuple(x + y for x, y in zip(tuple_a, tuple_b)))
+    a1, a2 = (tuple_a + (0, 0))[:2]
+    b1, b2 = (tuple_b + (0, 0))[:2]
+    return (a1 + b1, a2 + b2)
