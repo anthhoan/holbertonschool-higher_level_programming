f9f7398a86a92f5483a40deb65a93b20db2298f7
anthhoang <anthhoan@gmail.com>
2025-05-13 10:45:14 +1000

    print matrix

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -4,8 +4,8 @@ def print_matrix_integer(matrix=[[]]):
 
     for row in range(len(matrix)):
         for column in range(len(matrix[row])):
-            if len(matrix[column]) == ',':
+            if len(matrix[row]) == ',':
                 print("{:d}".format(matrix[row][column]), end="")
-            else:  
+            else:
                 print("{:d}".format(matrix[row][column]), end=" ")
         print()
