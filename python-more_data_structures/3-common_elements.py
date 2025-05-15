bb1e5b7393c1de3fa8a646aec79ab0ee0daaddfe
anthhoang <anthhoan@gmail.com>
2025-05-15 16:00:03 +1000

    different elements

--- python-more_data_structures/3-common_elements.py
+++ python-more_data_structures/3-common_elements.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
-# https://www.geeksforgeeks.org/python-print-common-elements-two-lists/
+
 def common_elements(set_1, set_2):
-    common = list(set(set_1) & set(set_2))
-    return (common)
+    return set_1 & set_2
