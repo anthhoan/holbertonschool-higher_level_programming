34fe26c24fd845b8a73d5c74b3e575cc49a56cb7
anthhoang <anthhoan@gmail.com>
2025-05-15 15:48:31 +1000

    common elements

---
+++ python-more_data_structures/3-common_elements.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+
+def common_elements(set_1, set_2):
+    common = list(set(set_1) & set(set_2))
+    return (common)
+
+# https://www.geeksforgeeks.org/python-print-common-elements-two-lists/
