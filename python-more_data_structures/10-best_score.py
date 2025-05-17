bed67597bf035b36ac15d27dcb454a7ffc5b58bc
anthhoan <anthhoan@gmail.com>
2025-05-17 16:23:17 +1000

    best score

--- python-more_data_structures/10-best_score.py
+++ python-more_data_structures/10-best_score.py
@@ -2,7 +2,7 @@
 
 def best_score(a_dictionary):
     if not a_dictionary:
+        # if not checks if the dictionary is empty
         return (None)
-
-    best = max(a_dictionary, key =a_dictionary.get)
+    best = max(a_dictionary, key = a_dictionary.get)
     return (best)
