8c4d93d9ff0e201a1c8c2165af760e420ce81698
anthhoang <anthhoan@gmail.com>
2025-05-19 11:08:27 +1000

    Update

--- python-more_data_structures/2-uniq_add.py
+++ python-more_data_structures/2-uniq_add.py
@@ -2,5 +2,5 @@
 
 def uniq_add(my_list=[]):
     result = sum(set(my_list))
-    # a set in python is an unordered collection of unique elements
+    # a 'set' in python is an unordered collection of unique elements
     return (result)
