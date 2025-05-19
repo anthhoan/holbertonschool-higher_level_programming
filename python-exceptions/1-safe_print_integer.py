20a4429ee220e06e90dbb660085053b72f854c13
anthhoang <anthhoan@gmail.com>
2025-05-19 11:52:37 +1000

    is int?

--- python-exceptions/1-safe_print_integer.py
+++ python-exceptions/1-safe_print_integer.py
@@ -2,8 +2,7 @@
 
 def safe_print_integer(value):
     try:
-        val = int(value)
-        print("{:d}".format(val))
+        print("{:d}".format(value))
         return (True)
     except (ValueError, TypeError):
         return (False)
