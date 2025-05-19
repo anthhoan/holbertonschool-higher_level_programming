c8b6a663a5f571146e1520e1b52b32b7e8341a83
anthhoang <anthhoan@gmail.com>
2025-05-19 11:44:20 +1000

    is int?

--- python-exceptions/1-safe_print_integer.py
+++ python-exceptions/1-safe_print_integer.py
@@ -1,12 +1,15 @@
 #!/usr/bin/python3
 
 def safe_print_integer(value):
+    # try:
+    #     if type(value) is int:
+    #         print("{:d}".format(value))
+    #         return (True)
+    #     return (False)
+    # except Exception:
+    #     return (False)
     try:
-        if type(value) is int:
-            print("{:d}".format(value))
-            return (True)
-        else:
-            if type(value) is str:
-                return (False)
-    except IndexError:
-        pass
+        print("{:d}".format(value))
+        return (True)
+    except (ValueError, TypeError):
+        return (False)
