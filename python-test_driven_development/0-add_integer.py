2ef4cfc5b90d4dc213f5e46fe02565762ea413dc
anthhoang <anthhoan@gmail.com>
2025-05-22 11:21:14 +1000

    TypeError

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -1,13 +1,13 @@
 #!/usr/bin/python3
 
 def add_integer(a, b=98):
+
+    a = int(a)
+    b = int(b)
+
     if type(a) not in (int, float):
         raise TypeError('a must be an integer')
     if type(b) not in (int, float):
         raise TypeError('b must be integer')
     
-    a = int(a)
-    b = int(b)
-
     return (a + b)
-
