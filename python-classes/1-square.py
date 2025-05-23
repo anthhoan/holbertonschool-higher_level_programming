a515e79f01086cad6414846ce3cb6d1dbc10e400
anthhoang <anthhoan@gmail.com>
2025-05-23 11:47:35 +1000

    size of Square

---
+++ python-classes/1-square.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""class Square with private instance attribute size"""
+
+
+class Square:
+    def __init__(self, size):
+        """__init__ initializes the square
+        
+        Attributes:
+            size: The private size of the square
+        """
+        self.__size = size
