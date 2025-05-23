a0723aabbe26f206ee3c82d183bb1375a331566d
anthhoang <anthhoan@gmail.com>
2025-05-23 13:16:45 +1000

    area of square

---
+++ python-classes/3-square.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""class Square with TypeError and ValueError(s)"""
+
+
+class Square:
+    """class Square"""
+    def __init__(self, size=0):
+        """__init__ initializes the square
+
+        Attributes:
+            size: The private size of a square
+
+        Errors:
+            TypeError: if size is not integer
+            ValueError: if size is less than 0
+        """
+        if type(size) is not int:
+            raise TypeError('size must be an integer')
+
+        if size < 0:
+            raise ValueError('size must be >= 0')
+
+        self.__size = size
+
+    def area(self):
+        """returns the area of the current square"""
+        return (self.__size ** 2)
