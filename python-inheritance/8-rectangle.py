b0164f8e082fb2b0cdab1e42ccceeb06ee4282d8
anthhoan <anthhoan@gmail.com>
2025-05-31 17:01:06 +1000

    class Rectangle(BaseGeometry)

--- python-inheritance/8-rectangle.py
+++ python-inheritance/8-rectangle.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
-BaseGeometry = __import__('7-base_geometry').BaseGeometry
 """a class Rectangle the inherits from BaseGeometry"""
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
 
 
 class Rectangle(BaseGeometry):
