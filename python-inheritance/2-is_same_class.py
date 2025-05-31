762843f249716073128223563abe00f3a45cabeb
anthhoan <anthhoan@gmail.com>
2025-05-31 12:39:34 +1000

    is same class

--- python-inheritance/2-is_same_class.py
+++ python-inheritance/2-is_same_class.py
@@ -3,5 +3,5 @@
 
 
 def is_same_class(obj, a_class):
-        """Returns true is instance is same"""
-        return (type(obj) is a_class)
+    """Returns true is instance is same"""
+    return (type(obj) is a_class)
