2895337fb05fc6e178ab9d56ebaffbdcaede3f14
anthhoan <anthhoan@gmail.com>
2025-05-31 13:01:48 +1000

    raise Exception message

---
+++ python-inheritance/6-base_geometry.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""Improve Geometry"""
+
+
+class BaseGeometry:
+    """class BaseGeometry"""
+
+    """Public instance method"""
+    def area(self):
+        """raise an Exception with a message"""
+        raise Exception('area() is not implemented')
