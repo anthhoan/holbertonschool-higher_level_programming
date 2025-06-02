696c8d526bf30a7139258340ea74e63a005c774c
anthhoang <anthhoan@gmail.com>
2025-06-02 13:44:58 +1000

    super function

--- python-inheritance/10-square.py
+++ python-inheritance/10-square.py
@@ -8,7 +8,7 @@ class Square(Rectangle):
     """class Square"""
     def __init__(self, size):
         """initializing a square"""
-        self.integer_validator("size", size)    
+        self.integer_validator("size", size)
         """integer validator raise typeError or valueError"""
         super().__init__(size, size)
         """super() function that refers to parent class"""
