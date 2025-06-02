e197c648bf361772c5b6ea06721eb17f84b8d66e
anthhoang <anthhoan@gmail.com>
2025-06-02 14:45:22 +1000

    inheritance

---
+++ python-inheritance/11-square.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""a class Square that inherits from Rectangle"""
+Rectangle = __import__('9-rectangle').Rectangle
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
+
+
+class Square(Rectangle):
+    """class Square"""
+    def __init__(self, size):
+        """initializing a square"""
+        self.integer_validator("size", size)
+        """integer validator raise typeError or valueError"""
+        super().__init__(size, size)
+        """super() function that refers to parent class"""
+        self.__size = size
+
+    def __str__(self):
+        """return a string square"""
+        return ("[Square] {}/{}".format(self.__size, self.__size))
