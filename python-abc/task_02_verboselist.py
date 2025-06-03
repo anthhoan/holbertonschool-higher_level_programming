53f09e36302919ff64abe9520bc94462d257b727
anthhoan <anthhoan@gmail.com>
2025-06-03 22:49:26 +1000

    verboselist

--- python-abc/task_02_verboselist.py
+++ python-abc/task_02_verboselist.py
@@ -1,18 +1,25 @@
-#!/usr/bin/python3 
+#!/usr/bin/python3
+"""Extending the Python List with Notifications"""
+
 
 class VerboseList(list):
+    """class Verbose that inherits built in list class"""
     def append(self, item):
+        """appending to a list"""
         super().append(item)
         print("Added [{}] to the list.".format(item))
 
     def extend(self, item):
+        """extend the list with number of items"""
         super().extend(item)
         print("Extended the list with [{}] items.".format(len(item)))
 
     def remove(self, item):
+        """remove item from list"""
         super().remove(item)
         print("Removed [{}] from the list.".format(item))
 
-    def pop(self, item):
-        super().pop(item)
+    def pop(self, index=-1):
+        """pop a index from a list"""
+        item = super().pop(index)
         print("Popped [{}] from the list.".format(item))
