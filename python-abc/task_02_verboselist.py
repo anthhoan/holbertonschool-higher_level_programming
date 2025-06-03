3954ee672dc97ee68ebfb4bdd52bd6ca3fa3a6b2
anthhoan <anthhoan@gmail.com>
2025-06-03 23:00:09 +1000

    verboselist

--- python-abc/task_02_verboselist.py
+++ python-abc/task_02_verboselist.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python3
-"""Extending the Python List with Notifications"""
+"""Extending the Python List with Notifications
+the built in list class has multiple methods such as
+append, extend, insert, remove, pop, clear, sort, reverse.
+We use super() because it calls on the original method() from the parent class List"""
 
 
 class VerboseList(list):
@@ -20,7 +23,9 @@ class VerboseList(list):
         print("Removed [{}] from the list.".format(item))
 
     def pop(self, index=-1):
-        """pop a index from a list"""
+        """pop a index from a list
+        index=-1 is a default that aligns with pythons built in behavior
+        the original pop() method removes and returns"""
         item = super().pop(index)
         print("Popped [{}] from the list.".format(item))
         return (item)
