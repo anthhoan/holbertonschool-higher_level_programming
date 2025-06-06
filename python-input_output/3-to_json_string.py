e0da0a54a0b660499533aa63feb5de4078db5dc9
anthhoang <anthhoan@gmail.com>
2025-06-06 13:49:38 +1000

    JSON dumps

--- python-input_output/3-to_json_string.py
+++ python-input_output/3-to_json_string.py
@@ -5,5 +5,5 @@ import json
 
 def to_json_string(obj):
     """dumps is used to convert a python object into a JSON string/
-    dumps with a s = s = string"""
+    dumps with a s = s = string`"""
     return json.dumps(obj)
