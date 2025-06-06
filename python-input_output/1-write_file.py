e28e6c7a5236891a213870645b183e56031005fe
anthhoang <anthhoan@gmail.com>
2025-06-06 12:45:34 +1000

    write file

--- python-input_output/1-write_file.py
+++ python-input_output/1-write_file.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
-"""A function that writes to a text file UTF-8 and returns the number of characters"""
+"""A function that writes to a text file UTF-8 
+and returns the number of characters"""
 
 
 def write_file(filename="", text=""):
