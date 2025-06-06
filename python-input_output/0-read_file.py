eb0995a66c69ee5289de0c26ee983424c2259458
anthhoang <anthhoan@gmail.com>
2025-06-06 12:27:37 +1000

    read file

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -1,6 +1,10 @@
 #!/usr/bin/python3
+"""A function that reads a text file UTF-8"""
+
 
 def read_file(filename=""):
+    """read_file function"""
     with open(filename, "r", encoding="utf-8") as f:
+        """opens the file with r = read"""
         print(f.read(), end="")
     return(f.read())
