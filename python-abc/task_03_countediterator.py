564384a26ea8d3046d18a4be3e3c96f4ee010aa2
anthhoan <anthhoan@gmail.com>
2025-06-09 15:53:48 +1000

    countediterator

--- python-abc/task_03_countediterator.py
+++ python-abc/task_03_countediterator.py
@@ -1,9 +1,25 @@
 #!/usr/bin/python3
+"""class CountedIterator that extends the built in iterator obtained/
+from the iter function."""
+
 
 class CountedIterator(iter):
-    def __init__(self, iter_object, counter):
-        self.iter_object = iter_object
-        self.counter = counter
+    """class CountedIterator"""
+    def __init__(self, some_iterable):
+        """two attributes, the iterator object, and a counter"""
+        self.iterator = iter(some_iterable)
+        self.count = 0
 
-    def iter_object(self):
-        
+    def get_count(self):
+        """return the current value of the counter"""
+        return self.count
+
+    def __next__(self):
+        """next method raises a StopIteration exception when there are no/
+        more items to iterate"""
+        try:
+            item = next(self.iterator)
+            self.count += 1
+            return (item)
+        except StopIteration:
+            raise StopIteration("No more items to iterate")
