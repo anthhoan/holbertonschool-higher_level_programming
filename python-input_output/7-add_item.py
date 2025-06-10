75e6eee46e81140c1885669d9b747908cf8eb0e2
anthhoang <anthhoan@gmail.com>
2025-06-10 11:20:49 +1000

    __dict__

---
+++ python-input_output/7-add_item.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+
+load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
+save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+
+if __name__ == "__main__":
+    def add_to_json_file(filename)
