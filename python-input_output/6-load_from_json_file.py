b38cc4360d2445dd92a2f36860e5d72ea657ddce
anthhoang <anthhoan@gmail.com>
2025-06-10 10:25:08 +1000

    json load

--- python-input_output/6-load_from_json_file.py
+++ python-input_output/6-load_from_json_file.py
@@ -1,6 +1,10 @@
 #!/usr/bin/python3
+"""function that creates an Object from JSON file"""
 import json
+
 
 def load_from_json_file(filename):
     with open(filename, "r", encoding="utf-8") as f:
+        """open filename with r = read only"""
         json.load(filename)
+        """json.loaf() takes a file object and returns the json object"""
