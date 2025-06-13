02c0a1c3e6d1294e8d1aad3ad0bd6c290306d635
anthhoan <anthhoan@gmail.com>
2025-06-13 12:02:47 +1000

    basic serialization

--- python-serialization/task_00_basic_serialization.py
+++ python-serialization/task_00_basic_serialization.py
@@ -1,9 +1,19 @@
 #!/usr/bin/python3
+"""Basic serialization
+Creating a basic serialization module that adds the functionality to serialize a
+Python dictionary to a JSON file and deserialize te JSON file to recreate
+the Python Dictionary"""
+import json
+
 
 def serialize_and_save_to_file(data, filename):
-    # Your code here to serialize and save data to the specified file
-    pass
+    """Serialize and save data to a specified file"""
+    with open(filename, "w", encoding="utf-8") as f:
+        file = json.dump(f)
+        return (f)
 
 def load_and_deserialize(filename):
-    # Your code here to load and deserialize data from the specified file
-    pass
+    """Load and deserialize data from the specified file"""
+    with open (filename, "r", encoding="utf-8") as f:
+        file = json.load(f)
+        return (f)
