ba0aa8cd8b20837bff2e6b4c2bc2bd1aaf0c69d8
anthhoan <anthhoan@gmail.com>
2025-06-13 12:18:01 +1000

    basic serialization

--- python-serialization/task_00_basic_serialization.py
+++ python-serialization/task_00_basic_serialization.py
@@ -10,7 +10,6 @@ def serialize_and_save_to_file(data, filename):
     """Serialize and save data to a specified file"""
     with open(filename, "w", encoding="utf-8") as f:
         file = json.dumps(f)
-        return (file)
 
 def load_and_deserialize(filename):
     """Load and deserialize data from the specified file"""
