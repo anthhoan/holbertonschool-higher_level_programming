f066a47ab12e1f34094c44be2a718420ae40b01c
anthhoan <anthhoan@gmail.com>
2025-06-26 14:25:35 +1000

    CREATE TABLE

--- SQL_introduction/4-first_table.sql
+++ SQL_introduction/4-first_table.sql
@@ -1,5 +1,5 @@
 -- Create a table called first_table in the current database in your MySQL server
-CREATE TABLE IF EXISTS first_table (
+CREATE TABLE IF NOT EXISTS first_table (
     column1 INT,
     column2 VARCHAR(255)
 );
