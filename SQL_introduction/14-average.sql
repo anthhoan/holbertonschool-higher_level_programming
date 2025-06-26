774239cdbc944bec63b542b432f359c413430224
anthhoan <anthhoan@gmail.com>
2025-06-26 17:27:32 +1000

    AVG

---
+++ SQL_introduction/14-average.sql
@@ -0,0 +1,7 @@
+-- Compute the score average of all records in the table second_table of the database hbtn_0c_0 in your MySQL server
+-- FROM second_table 
+-- WHERE score IS ADD
+-- SELECT AVG;
+
+SELECT AVG(score) average 
+FROM second_table;
