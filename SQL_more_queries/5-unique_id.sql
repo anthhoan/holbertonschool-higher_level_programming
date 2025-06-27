c4f75ddbb6872f4d97d480f176627fa65decfbeb
anthhoang <anthhoan@gmail.com>
2025-06-27 15:18:42 +1000

    UNIQUE id

--- SQL_more_queries/5-unique_id.sql
+++ SQL_more_queries/5-unique_id.sql
@@ -1,5 +1,5 @@
 -- Create the table unique_id on your MySQL server.
 CREATE TABLE IF NOT EXISTS unique_id (
-    id INT DEFAULT 1 AND UNIQUE,
+    id INT DEFAULT 1 UNIQUE,
     name VARCHAR(256)
 );
