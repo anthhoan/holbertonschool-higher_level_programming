7c8bcc602b38f41d1c2e83233c75935d017f72a5
anthhoang <anthhoan@gmail.com>
2025-06-27 14:44:13 +1000

    GRANT ALL PRIVILEGES

--- SQL_more_queries/1-create_user.sql
+++ SQL_more_queries/1-create_user.sql
@@ -1,5 +1,5 @@
 -- Create the MySQL server user user_0d_1
 CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' 
 IDENTIFIED BY 'user_0d_1';
-GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost' WITH GRANT OPTION;
 FLUSH PRIVILEGES;
