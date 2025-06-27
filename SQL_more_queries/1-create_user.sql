81e85aab5cd505da71c11767f1af44260e718b5d
anthhoang <anthhoan@gmail.com>
2025-06-27 14:35:56 +1000

    CREATE USER

--- SQL_more_queries/1-create_user.sql
+++ SQL_more_queries/1-create_user.sql
@@ -1,5 +1,5 @@
 -- Create the MySQL server user user_0d_1
 IF NOT EXIST 
 CREATE USER 'user_0d_1'@'localhost' 
-IDENTIFIED BY 'user_0d_1';
-GRANT CREATE, ALTER, DROP, INSERT, UPDATE, DELETE, SELECT, REFERENCES, RELOAD on *.* TO 'user_0d_1'@'localhost' WITH GRANT OPTION;
+IDENTIFIED BY 'user_0d_1'
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
