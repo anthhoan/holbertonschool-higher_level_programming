233bc2ebcdcba04eab2d1d36282e2ad1969b8ba5
anthhoang <anthhoan@gmail.com>
2025-07-01 11:57:28 +1000

    NUMBER OF SHOWS

--- SQL_more_queries/13-count_shows_by_genre.sql
+++ SQL_more_queries/13-count_shows_by_genre.sql
@@ -2,5 +2,5 @@
 SELECT tv_genres.name, tv_show_genres.genre_id
 from tv_genres
 INNER JOIN tv_show_genres
-ON tv_genres.genre_id = tv_show_genres.id
+ON tv_genres.genre_id = tv_show_genres.genre_id
 ORDER BY tv_show_genres.genre_id ASC;
