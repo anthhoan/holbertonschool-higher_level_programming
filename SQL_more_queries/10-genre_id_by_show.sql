fc8e6e4a566a9aaccd042a67fc0809f35f07087c
anthhoang <anthhoan@gmail.com>
2025-07-01 10:25:05 +1000

    tv_shows

--- SQL_more_queries/10-genre_id_by_show.sql
+++ SQL_more_queries/10-genre_id_by_show.sql
@@ -1,5 +1,5 @@
 --  Lists all shows contained in hbtn_0d_tvshows that have at least one genre linked
 SELECT tv_shows.title, tv_show_genres.genre_id
-from hbtn_0d_tvshows
+FROM tv_shows
 JOIN tv_shows.title ON genre_id
 ORDER BY tv_shows.title AND tv_show_genres.genre_id
