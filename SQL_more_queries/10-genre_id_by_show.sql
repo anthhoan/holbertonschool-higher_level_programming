494f72d5f6ff4b5eedb72477d27ef4df2791dbfa
anthhoang <anthhoan@gmail.com>
2025-07-01 11:26:17 +1000

    tv_shows

--- SQL_more_queries/10-genre_id_by_show.sql
+++ SQL_more_queries/10-genre_id_by_show.sql
@@ -2,5 +2,5 @@
 SELECT tv_shows.title, tv_show_genres.genre_id
 FROM tv_shows
 INNER JOIN tv_show_genres 
-ON tv_shows.title = tv_show_genres.title
+ON tv_shows.id = tv_show_genres.id
 ORDER BY tv_shows.title, tv_show_genres.genre_id ASC;
