98525cb036a7419779e5408f25eceb703936f34f
anthhoang <anthhoan@gmail.com>
2025-07-01 11:43:28 +1000

    IS NULL

--- SQL_more_queries/12-no_genre.sql
+++ SQL_more_queries/12-no_genre.sql
@@ -1,7 +1,6 @@
 -- Lists all shows contained in hbtn_0d_tvshows without a genre linked
 SELECT tv_shows.title, tv_show_genres.genre_id
 FROM tv_shows
-WHERE tv_show_genres IS NULL
 JOIN tv_show_genres
 ON tv_shows.id = tv_show_genres.show_id
 ORDER BY tv_shows.title, tv_show_genres.genre_id ASC;
