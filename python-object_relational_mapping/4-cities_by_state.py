21a7c2fe4df646563ab6eb08ddfda07f867c5df0
anthhoang <anthhoan@gmail.com>
2025-07-03 16:09:59 +1000

    Cities by States

---
+++ python-object_relational_mapping/4-cities_by_state.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+"""Script that lists all cities from the database hbtn_0e_4_usa"""
+import MySQLdb
+import sys
+
+
+def all_cities(username, password, database):
+    """Script to connect to database and run SQL query"""
+    db = MySQLdb.connect(
+        host="localhost",
+        port=3306,
+        user=username,
+        passwd=password,
+        db=database
+    )
+    cur = db.cursor()
+    cur.execute("SELECT cities.id, states.name, cities.name FROM cities "
+                "INNER JOIN states "
+                "ON cities.state_id = states.id "
+                "ORDER BY cities.id ASC;")
+    result = cur.fetchall()
+    for row in result:
+        print(row)
+    cur.close()
+    db.close()
+
+
+if __name__ == "__main__":
+    """Main function"""
+    all_cities(sys.argv[1], sys.argv[2], sys.argv[3])
