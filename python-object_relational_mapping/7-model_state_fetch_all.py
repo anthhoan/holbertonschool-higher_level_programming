f8920091322deeab9f64b66acfc86fa5b106cd8e
anthhoang <anthhoan@gmail.com>
2025-07-07 16:47:49 +1000

    fetch all sqlalchemy

--- python-object_relational_mapping/7-model_state_fetch_all.py
+++ python-object_relational_mapping/7-model_state_fetch_all.py
@@ -16,6 +16,6 @@ if __name__ == "__main__":
     session = Session()
     """connecting to the session"""
     for state in session.query(State).order_by(State.id).all():
-            print("{}: {}".format(state.id, state.name))
+        print("{}: {}".format(state.id, state.name))
     session.close()
     """close the session"""
