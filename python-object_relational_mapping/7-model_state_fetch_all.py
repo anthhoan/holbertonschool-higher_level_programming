38b0ea4f521137795150e6f058029ea78db1e6b2
anthhoang <anthhoan@gmail.com>
2025-07-07 16:44:47 +1000

    fetch all sqlalchemy

--- python-object_relational_mapping/7-model_state_fetch_all.py
+++ python-object_relational_mapping/7-model_state_fetch_all.py
@@ -9,7 +9,7 @@ from sqlalchemy.orm import sessionmaker
 if __name__ == "__main__":
     """Main function"""
     engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'
-                            .format(sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True)
+                    .format(sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True)
     """create the engine"""
     """pool_pre_ping=True tests the database connections before continuing"""
     Base.metadata.create_all(engine)
