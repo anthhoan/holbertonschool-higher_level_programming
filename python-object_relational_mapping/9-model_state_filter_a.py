1c3d4a63bbcef7bea7c0e00ef41f71b4d46e97ce
anthhoang <anthhoan@gmail.com>
2025-07-07 17:33:37 +1000

    contains letter 'a'

--- python-object_relational_mapping/9-model_state_filter_a.py
+++ python-object_relational_mapping/9-model_state_filter_a.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
-
+"""Lists all State objects that contain the letter 'a'
+from the database hbtn_0e_6_usa using SQLAlchemy"""
 import sys
 from model_state import Base, State
 from sqlalchemy import create_engine
