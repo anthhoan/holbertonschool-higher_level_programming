0112600b9551a82bc404ba9fd697647af5e52627
anthhoang <anthhoan@gmail.com>
2025-07-07 17:38:55 +1000

    contains letter 'a'

--- python-object_relational_mapping/9-model_state_filter_a.py
+++ python-object_relational_mapping/9-model_state_filter_a.py
@@ -18,7 +18,7 @@ if __name__ == "__main__":
     session = Session()
     """Create actual session"""
     for letter_a in session.query(State).order_by(State.id)\
-        .filter(State.name.contains('a')).all():
+            .filter(State.name.contains('a')).all():
         print("{}: {}".format(letter_a.id, letter_a.name))
         """Using the session with SQLAlchemy"""
     session.close()
