34f15cf0ab37825e1fc00853f5e2965715775e10
anthhoang <anthhoan@gmail.com>
2025-07-08 10:13:25 +1000

    Add to database

---
+++ python-object_relational_mapping/11-model_state_insert.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""Print the first State object
+from the database hbtn_0e_6_usa with SQLAlchemy"""
+import sys
+from model_state import Base, State
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+if __name__ == "__main__":
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'
+                           .format(sys.argv[1], sys.argv[2], sys.argv[3]))
+    """Connect to the database"""
+    Base.metadata.create_all(engine)
+    """Create tables if they don't exist"""
+    Session = sessionmaker(bind=engine)
+    """Create session factory"""
+    session = Session()
+    """Create actual session"""
+    new_state = State(name = "Louisiana")
+    """Using the session with SQLAlchemy"""
+    session.add(new_state)
+    """Adding a new object to the database"""
+    print(new_state.id)
+    session.commit()
+    """Pushing changes to the database"""
+    session.close()
+    """Closing the session"""
