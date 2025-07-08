31aae6696d8d798e2b6fef5e7ad9ed2517acb1b4
anthhoang <anthhoan@gmail.com>
2025-07-08 11:42:43 +1000

    Cities in States

--- python-object_relational_mapping/model_city.py
+++ python-object_relational_mapping/model_city.py
@@ -2,16 +2,10 @@
 """Class definition of Cities and an instance Base"""
 from sqlalchemy import Column, Integer, String, ForeignKey
 from sqlalchemy.ext.declarative import declarative_base
+from model_state import Base
 
 
 Base = declarative_base()
-
-
-class State(Base):
-    """State class that inherits from Base"""
-    __tablename__ = 'states'
-    id = Column(Integer, primary_key=True, nullable=False, autoincrement=True)
-    name = Column(String(128), nullable=False)
 
 
 class City(Base):
