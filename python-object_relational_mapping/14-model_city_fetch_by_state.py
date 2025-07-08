ee36a4b7667f42972c5dc5d37902a51b59fb7364
anthhoang <anthhoan@gmail.com>
2025-07-08 11:37:38 +1000

    Cities in States

---
+++ python-object_relational_mapping/14-model_city_fetch_by_state.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""List all State objects from the database hbtn_0e_6_usa with SQLAlchemy"""
+import sys
+from model_state import Base, State
+from model_city import Base, Cities
+from sqlalchemy import (create_engine)
+from sqlalchemy.orm import sessionmaker
+
+
+if __name__ == "__main__":
+    """Main function"""
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'
+                           .format(sys.argv[1], sys.argv[2], sys.argv[3]))
+    """Connect to the database"""
+    Base.metadata.create_all(engine)
+    """Create tables if they don't exist"""
+    Session = sessionmaker(bind=engine)
+    """Create session factory"""
+    session = Session()
+    """Create actual session"""
+    results = session.query(State, Cities).join(Cities, State.id == Cities.state_id)\
+        .order_by(State.id, Cities.id).all()
+    for state, cities in results:
+        print("{}: ({}) {}".format(state.name, cities.id, cities.name))
+    """Using the session with SQLAlchemy"""
+    session.close()
+    """Closing the session"""
---
+++ python-object_relational_mapping/model_city.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""Class definition of Cities and an instance Base"""
+from sqlalchemy import Column, Integer, String, ForeignKey
+from sqlalchemy.ext.declarative import declarative_base
+
+
+Base = declarative_base()
+
+class State(Base):
+    """State class that inherits from Base"""
+    __tablename__ = 'states'
+    id = Column(Integer, primary_key=True, nullable=False, autoincrement=True)
+    name = Column(String(128), nullable=False)
+
+
+class Cities(Base):
+    __tablename__ = 'cities'
+    id = Column(Integer, primary_key=True, nullable=False, autoincrement=True)
+    name = Column(String(128), nullable=False)
+    state_id = Column(Integer, ForeignKey('states.id'), nullable=False)
