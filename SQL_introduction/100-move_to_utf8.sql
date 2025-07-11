c3d39c568304f17682c21789217afedd4dc2714f
anthhoang <anthhoan@gmail.com>
2025-07-11 13:06:28 +1000

    Convert database to UTF

--- SQL_introduction/100-move_to_utf8.sql
+++ SQL_introduction/100-move_to_utf8.sql
@@ -1,8 +1,17 @@
 -- Converts hbtn_0c_0 database to UTF8 (utf8mb4, collate utf8mb4_unicode_ci) in your MySQL server
+
+-- Converts the database to UTF
 ALTER DATABASE hbtn_0c_0
 CHARACTER SET utf8mb4
 COLLATE utf8mb4_unicode_ci;
 
+-- Converts the table to UTF
 ALTER TABLE first_table
+CHARACTER SET utf8mb4
+COLLATE utf8mb4_unicode_ci;
+
+-- Converts the 'name' column to UTF
+ALTER TABLE first_table
+MODIFY name VARCHAR(255)
 CHARACTER SET utf8mb4
 COLLATE utf8mb4_unicode_ci;
