e1e8beef78d89c99c3ac70980ee8610083522d27
anthhoang <anthhoan@gmail.com>
2025-07-11 13:18:12 +1000

    Convert Database, Table and Column to UTF

--- SQL_introduction/100-move_to_utf8.sql
+++ SQL_introduction/100-move_to_utf8.sql
@@ -1,19 +1,19 @@
--- Converts hbtn_0c_0 database to UTF8 (utf8mb4, collate utf8mb4_unicode_ci) in your MySQL server
+-- Script to convert hbtn_0c_0 database to UTF8 (utf8mb4, collate utf8mb4_unicode_ci)
 
--- Converts the database to UTF
+-- Convert the database to UTF8
 ALTER DATABASE hbtn_0c_0
 CHARACTER SET utf8mb4
 COLLATE utf8mb4_unicode_ci;
 
--- Selects a database to use
+-- Select the database to use
 USE hbtn_0c_0;
 
--- Converts the table to UTF
+-- Convert the table to UTF8
 ALTER TABLE first_table
 CONVERT TO CHARACTER SET utf8mb4
 COLLATE utf8mb4_unicode_ci;
 
--- Converts the 'name' column to UTF
+-- Convert the 'name' field to UTF8
 ALTER TABLE first_table
 MODIFY name VARCHAR(256)
 CHARACTER SET utf8mb4
