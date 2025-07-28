f8adef2f1bcb135a5c37ed77774020137e96f671
anthhoang <anthhoan@gmail.com>
2025-07-28 13:41:04 +1000

    Click and turn red

---
+++ javascript-dom_manipulation/1-script.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+
+document.addEventListener("DOMContentLoaded", function () {
+    const redHeader = document.querySelector("#red_header");
+    const header = document.querySelector("header");
+
+    if (redHeader && header) {
+        redHeader.addEventListener("click", function () {
+            header.style.color = "#FF0000";
+        });
+    }
+})
